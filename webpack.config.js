d410d957e87e60ce8c31c75f6b9d0c4affc33d72
uotanklein <francferdinant2@gmail.com>
2024-10-24 12:16:05 +0300

    upd

--- webpack.config.js
+++ webpack.config.js
@@ -11,68 +11,68 @@ const __dirname = path.dirname(__filename);
 const isProduction = process.env.NODE_ENV === 'production';
 
 const config = {
-    entry: {
-        filename: path.resolve(__dirname, 'src/index.js'),
+  entry: {
+    filename: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    open: true,
+    host: 'localhost',
+    port: 9000,
+    compress: true,
+    hot: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
     },
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-        port: 9000,
-        compress: true,
-        hot: true,
-        static: {
-            directory: path.join(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
         },
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: path.resolve(__dirname, 'index.html'),
-        }),
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+        type: 'asset',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                        presets: ['@babel/preset-env'],
-                    },
-                },
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-        ],
+  },
+  resolve: {
+    alias: {
+      yup: path.resolve(__dirname, 'node_modules/yup/index.js'),
+      'on-change': path.resolve(__dirname, 'node_modules/on-change/index.js'),
+      i18next: path.resolve(__dirname, 'node_modules/i18next/index.js'),
+      axios: path.resolve(__dirname, 'node_modules/axios/index.js'),
     },
-    resolve: {
-        alias: {
-            yup: path.resolve(__dirname, 'node_modules/yup/index.js'),
-            'on-change': path.resolve(__dirname, 'node_modules/on-change/index.js'),
-            i18next: path.resolve(__dirname, 'node_modules/i18next/index.js'),
-            axios: path.resolve(__dirname, 'node_modules/axios/index.js'),
-        },
-        extensions: ['.js', '.jsx'],
-    },
+    extensions: ['.js', '.jsx'],
+  },
 };
 
 export default () => {
-    if (isProduction) {
-        config.mode = 'production';
+  if (isProduction) {
+    config.mode = 'production';
 
-        config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
-    } else {
-        config.mode = 'development';
-    }
-    return config;
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+  } else {
+    config.mode = 'development';
+  }
+  return config;
 };
